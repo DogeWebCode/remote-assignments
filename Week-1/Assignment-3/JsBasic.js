2965e0fbfecabfe10ed9baf24df88a44af3a3435
柴犬王 <70188364+DogeWebCode@users.noreply.github.com>
2024-06-19 17:34:39 +0800

    ✔修改toNumber function

--- Week-1/Assignment-3/JsBasic.js
+++ Week-1/Assignment-3/JsBasic.js
@@ -8,16 +8,9 @@ function countAandB(input) {
 }
 function toNumber(input) {
   // your code here
-  //先將Array to String，再用迴圈把Array裡的字母都轉成數字，主要是charCodeAt沒辦法直接針對Array使用。
-  let str = input.toString();
-  //此為將逗點改空格，因為轉換的時候也會將String中的","變Acsii Code。
-  let replaced_str = str.split(",").join("");
-  let result = [];
-  // 用charCodeAt來將英文轉成ASCii Code，減96是因為"a"在ASCii Code的Decimal為97，相減後得1。
-  for (let i = 0; i < input.length; i++) {
-    result[i] = replaced_str.charCodeAt(i) - 96;
-  }
-  return result;
+  // 用map遍歷帶進來的陣列，並將陣列內的字母轉ASCii Code。
+  const arr = input.map((item) => item.charCodeAt() - 96);
+  return arr;
 }
 let input1 = ["a", "b", "c", "a", "c", "a", "c"];
 console.log(countAandB(input1)); // should print 4 (3 ‘a’ letters  and 1 ‘b’ letter)
