9a90fdc7dbd9bc947a698e8bfde5fb047e52229a
柴犬王 <70188364+DogeWebCode@users.noreply.github.com>
2024-06-27 19:57:02 +0800

    ✔作業完成

--- Week-2/Assignment-4/js/script.js
+++ Week-2/Assignment-4/js/script.js
@@ -1,8 +1,19 @@
-const welcomeMsg = document.querySelector(".welcome");
+const welcomeMsg = document.querySelector(".welcome h1");
+const contenbox = document.querySelector(".contenbox_hidden");
 function changeText() {
-  if (welcomeMsg.innerText === "Welcome Message") {
-    welcomeMsg.innerHTML = `<h1>Have a Good Time!</h1>`;
+  if (welcomeMsg.textContent === "Welcome Message") {
+    welcomeMsg.textContent = "Have a Good Time!";
   } else {
-    welcomeMsg.innerHTML = `<h1>Welcome Message</h1>`;
+    welcomeMsg.textContent = "Welcome Message";
+  }
+}
+
+function btn_Toggle() {
+  if (contenbox.classList.contains("contenbox_hidden")) {
+    contenbox.classList.remove("contenbox_hidden");
+    contenbox.classList.add("contenbox_show");
+  } else {
+    contenbox.classList.remove("contenbox_show");
+    contenbox.classList.add("contenbox_hidden");
   }
 }
