05ac227a32e49280feb0104b83a0d6407f5ed506
柴犬王 <70188364+DogeWebCode@users.noreply.github.com>
2024-07-02 13:22:29 +0800

    ✔作業完成

--- Week-3/Assignment/src/main/java/com/example/demo/Controller/Controller.java
+++ Week-3/Assignment/src/main/java/com/example/demo/Controller/Controller.java
@@ -1,18 +1,27 @@
 package com.example.demo.Controller;
 
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseCookie;
+import org.springframework.web.bind.annotation.CookieValue;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 
 @RestController
 public class Controller {
     @RequestMapping(value = "/data", method = RequestMethod.GET)
-    public String numberSum(
-            @RequestParam(value = "num", required = true, defaultValue = "") String num) {
+    public String numberSum(@RequestParam(value = "num", required = true, defaultValue = "") String num) {
         //中括號用於定義character class，\d是代表0~9，*代表0或多個數字。
-        boolean isInteger = num.matches("^[-+]?[\\d]*$");
+        boolean isInteger = num.matches("^[\\d]*$");
         if (num.isEmpty()) {
             return "Lack of Parameter";
         } else if (!isInteger) {
@@ -34,6 +43,46 @@ public class Controller {
             } catch (NumberFormatException e) {
                 return "Too Large Number";
             }
+        }
+    }
+
+    @GetMapping("/trackName")
+    public String setCookie(@RequestParam("name") String inputName, HttpServletResponse response) throws IOException {
+        String encodedName = URLEncoder.encode(inputName, StandardCharsets.UTF_8);
+        ResponseCookie resCookie = ResponseCookie.from("name", encodedName)
+                .httpOnly(true)
+                .maxAge(60 * 60 * 24 * 365)
+                .path("/")
+                .domain("localhost")
+                .build();
+        response.addHeader("Set-Cookie", resCookie.toString());
+        response.sendRedirect("/myName");
+        return "cookie set";
+    }
+
+    @GetMapping("/myName")
+    public ResponseEntity<String> readCookie(@CookieValue(name = "name", required = false) String cookieName) {
+        if (cookieName == null) {
+            return ResponseEntity.ok().body(
+                """
+                    <html lang="en">
+                    <head>
+                        <meta charset="UTF-8">
+                        <title>Cookie Demo</title>
+                    </head>
+                    <body>
+                    <h1>Cookie Demo</h1>
+                    <form action="/trackName">
+                        <label for="nameInput">Enter your name：</label>
+                        <input type="text" name="name" id="nameInput" required>
+                        <input type="submit" value="submit" id="submitBtn">
+                    </form>
+                    </body>
+                    </html>
+                """
+            );
+        } else {
+            return ResponseEntity.ok().body("Your name is: " + cookieName);
         }
     }
 }
