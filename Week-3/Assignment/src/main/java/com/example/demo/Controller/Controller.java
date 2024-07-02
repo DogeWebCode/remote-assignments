0eae325493135a95d36891af27f139e40652af32
柴犬王 <70188364+DogeWebCode@users.noreply.github.com>
2024-07-02 09:04:03 +0800

    檔案挪位

--- Week-3/Assignment/src/main/java/com/example/demo/Controller/Controller.java
+++ Week-3/Assignment/src/main/java/com/example/demo/Controller/Controller.java
@@ -4,15 +4,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
+
 
 @RestController
 public class Controller {
     @RequestMapping(value = "/data", method = RequestMethod.GET)
     public String numberSum(
             @RequestParam(value = "num", required = true, defaultValue = "") String num) {
-        //正則表達由"^"開始，"$"結束，中括號用於定義character class，\d是代表0~9，*代表0或多個數字。
-        boolean isInteger = num.matches("^[\\d]*$");
+        //中括號用於定義character class，\d是代表0~9，*代表0或多個數字。
+        boolean isInteger = num.matches("^[-+]?[\\d]*$");
         if (num.isEmpty()) {
             return "Lack of Parameter";
         } else if (!isInteger) {
@@ -21,7 +21,7 @@ public class Controller {
             try {
                 // 用long type擴大數字範圍，盡量避免溢位情況發生。
                 long number = Long.parseLong(num);
-                // (1 + 2 + 3 +...+ N) = N * (N + 1) / 2 => 等差數列公式
+                // (1 + 2 + 3 +...+ N) = N * (N + 1) / 2 => 求和公式
                 // 此公式只會跑一次乘法、一次加法、一次除法，與n無關，時間複雜度為O(1)，用nanotime看都是200ns左右。
                 long total = number * (number + 1) / 2;
 
