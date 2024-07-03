b850bfbac192aa5f7b7add763c1276cb983a6b4f
柴犬王 <70188364+DogeWebCode@users.noreply.github.com>
2024-07-03 16:24:42 +0800

    ✔Add Html Input Regex

--- Week-3/Assignment/src/main/java/com/example/demo/Controller/nameController.java
+++ Week-3/Assignment/src/main/java/com/example/demo/Controller/nameController.java
@@ -1,18 +1,17 @@
 package com.example.demo.Controller;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
-@RestController
+@Controller
 public class nameController {
     @GetMapping("/trackName")
     public String setCookie(@RequestParam("name") String inputName, HttpServletResponse response) throws IOException {
@@ -25,13 +24,23 @@ public class nameController {
                 .domain("localhost")
                 .build();
         response.addHeader("Set-Cookie", resCookie.toString());
-        response.sendRedirect("/myName");
-        return "cookie set";
+        return "redirect:/myName";
     }
+
+    //practice post
+    /*
+    @PostMapping("/trackName")
+    public String trackName(@RequestParam String name, HttpServletResponse response) {
+        Cookie cookie = new Cookie("name", name);
+        cookie.setMaxAge(24 * 60 * 60); // 設置 cookie 的有效期為一天
+        response.addCookie(cookie);
+        return "redirect:/myName";
+    }
+    */
 
     @GetMapping("/myName")
     public ResponseEntity<String> readCookie(@CookieValue(name = "name", required = false) String cookieName) {
-        if (cookieName == null) {
+        if (cookieName == null || cookieName.isEmpty()) {
             return ResponseEntity.ok().body(
                     """
                         <html lang="en">
@@ -41,9 +50,9 @@ public class nameController {
                         </head>
                         <body>
                         <h1>Cookie Demo</h1>
-                        <form action="/trackName">
+                        <form action="/trackName" method="get">
                             <label for="nameInput">Enter your name：</label>
-                            <input type="text" name="name" id="nameInput" required>
+                            <input type="text" name="name" id="nameInput" pattern="[\u4e00-\u9fa5a-zA-Z]+" required>
                             <input type="submit" value="submit" id="submitBtn">
                         </form>
                         </body>
